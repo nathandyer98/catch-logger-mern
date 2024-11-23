513bd53d1d574a971476297619982e2a81f278e5
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-11-23 21:06:30 +0000

    added auth check route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -113,3 +113,12 @@ export const updateProfile = async (req, res) => {
         res.status(500).json({ message: "Internal server error" });
     }   
 }
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
+import { signup, login, logout, updateProfile, checkAuth } from "../controllers/auth.controller.js";
 import { authenticatedRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -10,5 +10,7 @@ router.post("/login", login)
 router.post("/logout", logout)
 
 router.put("/update-profile", authenticatedRoute, updateProfile)
+
+router.get("/check", authenticatedRoute, checkAuth)
 
 export default router;
