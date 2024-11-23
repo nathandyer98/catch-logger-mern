0509c2bf8bd52adc8b9a7a534a51e31478b52fdb
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-11-23 14:06:01 +0000

    mongo db connection set up

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
 
 import authRoute from "../routes/auth.route.js";
+import { connectDB } from "../lib/db.js";
+import { connect } from "mongoose";
 
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT || 5001;
 
 app.use("/api/auth", authRoute);
 
 
-app.listen(5001, () => {
-    console.log("Server is running on port 5001");
+app.listen(PORT, () => {
+    console.log("Server is running on port:",PORT);
+    connectDB();
 })
