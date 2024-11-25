7ab8c2eea0ffdb470f52189b11f99c683dff6145
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-11-25 18:31:11 +0000

    added api-client and authStore basic function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -1,3 +1,44 @@
 import { create } from "zustand";
+import { axiosInstance } from "../services/api-client";
 
-export const useAuthStore = create(() => {});
+export interface User {
+    _id: string;
+    email: string;
+    fullName: string;
+    password: string;
+    profilePic?: string;
+    createdAt: string;
+    updatedAt: string;
+  }
+
+interface AuthState {
+    authenticatedUser: User | null;
+    isSigningUp: boolean;
+    isLoggingIn: boolean;
+    isUpdatingProfile: boolean;
+    isCheckingAuth: boolean;
+    checkAuth: () => Promise<void>;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+    authenticatedUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+
+    isCheckingAuth: false,
+
+
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/auth/check-auth");
+            set({authenticatedUser: res.data});
+        } catch (error) {
+            console.log("Error in checkAuth controller", error);
+            set({authenticatedUser: null});
+            
+        } finally {
+            set({isCheckingAuth: false});
+        }
+    }
+}));
