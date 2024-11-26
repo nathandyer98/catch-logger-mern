1eda246927678deef396adfe71eb3fcb775fcbda
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-11-26 22:25:06 +0000

    removed unused code

--- frontend/src/components/OceanBackground.tsx
+++ frontend/src/components/OceanBackground.tsx
@@ -1,12 +1,6 @@
 const OceanBackground = () => {
   return (
     <div className="fixed inset-0 z-[-1] bg-[#0A2342] max-h-screen overflow-x-hidden overflow-y-hidden">
-      <linearGradient id="oceanDepthGradient" x1="0%" y1="0%" x2="0%" y2="100%">
-        <stop offset="25%" stopColor="#0A2342" />
-        <stop offset="80%" stopColor="#125D98" />
-        <stop offset="100%" stopColor="#1E4A7B" />
-      </linearGradient>
-
       {[...Array(100)].map((_, index) => {
         //Amount of bubbles
         const size = Math.random() * 10 + 3;
