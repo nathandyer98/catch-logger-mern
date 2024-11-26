db50da4c7bae7a4798e272602bc770aba1538c06
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-11-26 21:09:45 +0000

    minor tweaks
    +updated positioning of container
    +changed updating profile events to mouseOver and mouseLeave

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -31,8 +31,8 @@ const ProfilePage = () => {
   };
 
   return (
-    <div className="h-screen pt-20">
-      <div className="max-w-2xl mx-auto p-4 py-8">
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="w-full max-w-2xl mx-auto p-4 py-8">
         <div className="bg-base-200 rounded-xl p-6 space-y-8">
           <div className="text-center">
             <h1 className="text-2xl font-semibold ">Profile</h1>
@@ -101,8 +101,8 @@ const ProfilePage = () => {
                 </span>
               </div>
               <div
-                onBlur={() => setIsUpdatingName(false)}
-                onClick={() => setIsUpdatingName(true)}
+                onMouseLeave={() => setIsUpdatingName(false)}
+                onMouseOver={() => setIsUpdatingName(true)}
               >
                 {isUpdatingName && (
                   <input
@@ -124,7 +124,7 @@ const ProfilePage = () => {
                 <Mail className="w-4 h-4" />
                 Email Address
               </div>
-              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border pointer-events-none">
                 {authenticatedUser?.email}
               </p>
             </div>
