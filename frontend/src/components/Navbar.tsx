e13a55e5ec3bff98d20c8a008389abd33df3a785
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-11-26 17:20:08 +0000

    Top nav bar component created
    So far only contains logo and settings. Need to add authenticated user buttons

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,38 @@
-const Navbar = () => {
-  return <div>NavBar</div>;
-};
+import { Link } from "react-router-dom";
+import { FishSymbol, Settings } from "lucide-react";
 
-export default Navbar;
+const NavBar = () => {
+  return (
+    <header className="bg-opacity-0 fixed w-full top-0 z-40">
+      <div className="container mx-auto px-4 h-16">
+        <div className="flex items-center justify-between h-full">
+          <div className="flex items-center gap-8">
+            <Link
+              to="/"
+              className="flex items-center gap-2.5 hover:brightness-125 transition-all"
+            >
+              <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
+                <FishSymbol className="w-5 h-5 text-primary" />
+              </div>
+              <h1 className="text-lg font-bold">Catch Logger</h1>
+            </Link>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <Link
+              to={"/settings"}
+              className={`
+              btn btn-sm gap-2 transition-colors
+              
+              `}
+            >
+              <Settings className="w-4 h-4" />
+              <span className="hidden sm:inline">Settings</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default NavBar;
