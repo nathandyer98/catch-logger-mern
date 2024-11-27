c7267aeca74293149f5609c703a20efc39e68eef
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-11-27 19:19:28 +0000

    added navigation to buttons (settings page)
    +only settings works for now, more to come once the pages have been made

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,15 +1,20 @@
 import { Bell, Home, MessageSquare, Settings, Users } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="">
       {/* Navigation Links */}
       <nav className="flex flex-col rounded-2xl w-auto">
-        <NavItem label="Home" icon={<Home size={24} />} />
-        <NavItem label="Groups" icon={<Users size={24} />} />
-        <NavItem label="Notifications" icon={<Bell size={24} />} />
-        <NavItem label="Messages" icon={<MessageSquare size={24} />} />
-        <NavItem label="Settings" icon={<Settings size={24} />} />
+        <NavItem label="Home" icon={<Home size={24} />} path="/" />
+        <NavItem label="Groups" icon={<Users size={24} />} path="/" />
+        <NavItem label="Notifications" icon={<Bell size={24} />} path="/" />
+        <NavItem label="Messages" icon={<MessageSquare size={24} />} path="/" />
+        <NavItem
+          label="Settings"
+          icon={<Settings size={24} />}
+          path="/settings"
+        />
       </nav>
     </div>
   );
@@ -19,18 +24,19 @@ const Sidebar = () => {
 type NavItemProps = {
   label: string;
   icon: React.ReactNode;
+  path: string;
 };
 
-const NavItem: React.FC<NavItemProps> = ({ label, icon }) => {
+const NavItem: React.FC<NavItemProps> = ({ label, icon, path }) => {
   return (
-    <a
-      href="#"
+    <Link
+      to={path}
       className="flex items-center gap-3 px-4 py-3 transition-all duration-400 hover:bg-base-300 hover:rounded-xl"
     >
       <div className="text-primary">{icon}</div>
       {/* Hide label on small screens */}
       <span className="text-sm font-semibold hidden md:block">{label}</span>
-    </a>
+    </Link>
   );
 };
 
