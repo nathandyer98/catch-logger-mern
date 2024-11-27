7a9dea11789fac329442fcb0614c4984950b824b
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-11-27 22:28:06 +0000

    initialised catches interface

---
+++ frontend/src/types/catches.ts
@@ -0,0 +1,10 @@
+export interface Catch {
+    _id: string;
+    species: string;
+    weight: number;
+    img: string;
+    dateCaught: Date;
+    location: string;
+    comments: string;
+    createdAt: Date;
+}
