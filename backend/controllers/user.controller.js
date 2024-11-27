8052c27358111f38f4b535df9183f075887643fb
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-11-27 22:27:30 +0000

    improved fetch user api response
    +to fit more closely with the frontend needs

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -5,14 +5,17 @@ export const getUserProfile = async (req, res) => {
     try {
         const userId = req.params.id || req.user._id;
         const user = await User.findById(userId)
-        .select("fullName profilePic friends catches")
+        .select("fullName profilePic catches")
         .populate("catches");
 
         if (!user) {
             return res.status(404).json({ message: "User not found." });
         }
 
-        res.status(200).json({ user, friends: user.friends.length });
+        const friends = await User.findById(userId).select("friends")
+
+
+        res.status(200).json({ _id: user._id, fullName: user.fullName, profilePic: user.profilePic, friends: friends.friends.length, catches: user.catches });
     } catch (error) {
         console.log("Error in getUserProfile controller", error);
         res.status(500).json({ message: "Internal server error" });
