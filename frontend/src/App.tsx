c2e9b54acbc97996ae0530b24f9c70d6cff0147d
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-11-27 19:51:43 +0000

    moved sidebar into main app component
    +the sidebar now persist through the whole application.
    +main app container is a grid

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import OceanBackground from "./components/OceanBackground";
 import { Toaster } from "react-hot-toast";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   const { authenticatedUser, checkAuth, isCheckingAuth } = useAuthStore();
@@ -30,36 +31,48 @@ const App = () => {
   return (
     <div className="container mx-auto px-4">
       <Navbar />
+      <div className="text-base-content gap-2 grid grid-cols-6">
+        <div className="col-span-1 pb-10">
+          <Sidebar />
+        </div>
+        <div className="col-span-4 pb-10">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                authenticatedUser ? <HomePage /> : <Navigate to="/login" />
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+                !authenticatedUser ? <SignUpPage /> : <Navigate to="/" />
+              }
+            />
+            <Route
+              path="/login"
+              element={!authenticatedUser ? <LoginPage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile"
+              element={
+                authenticatedUser ? <HomePage /> : <Navigate to="/login" />
+              }
+            />
+            <Route
+              path="/settings"
+              element={
+                authenticatedUser ? <SettingsPage /> : <Navigate to="/login" />
+              }
+            />
+          </Routes>
+        </div>
 
-      <Routes>
-        <Route
-          path="/"
-          element={authenticatedUser ? <HomePage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/signup"
-          element={!authenticatedUser ? <SignUpPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/login"
-          element={!authenticatedUser ? <LoginPage /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/profile"
-          element={authenticatedUser ? <HomePage /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/settings"
-          element={
-            authenticatedUser ? <SettingsPage /> : <Navigate to="/login" />
-          }
-        />
-      </Routes>
+        <Toaster />
 
-      <Toaster />
-
-      <div className="relative max-h-screen flex items-center justify-center">
-        <OceanBackground />
+        <div className="relative max-h-screen flex items-center justify-center">
+          <OceanBackground />
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,17 +1,7 @@
 import React from "react";
-import Sidebar from "../components/Sidebar";
 
 const HomePage: React.FC = () => {
-  return (
-    <div className="text-base-content gap-2 grid grid-cols-6 md:grid-cols-4">
-      {/* Sidebar */}
-      <div className="col-span-1 pb-10">
-        <Sidebar />
-      </div>
-
-      {/* Main Content */}
-    </div>
-  );
+  return <div className=""></div>;
 };
 
 export default HomePage;
