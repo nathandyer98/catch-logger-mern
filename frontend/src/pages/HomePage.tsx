77b57b8022ed2f1c2ef496446f12876b78a43c93
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-11-27 12:46:36 +0000

    updated homepage with gridlayout
    +imported sidebar for testing

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,17 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
+import React from "react";
+import Sidebar from "../components/Sidebar";
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="text-base-content gap-2 grid grid-cols-6 md:grid-cols-4">
+      {/* Sidebar */}
+      <div className="col-span-1 pb-10">
+        <Sidebar />
+      </div>
+
+      {/* Main Content */}
+    </div>
+  );
 };
 
 export default HomePage;
