ec38204b423d369cecbf938f63ecb425de9a62bc
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-11-27 12:20:10 +0000

    improved navbar responsiveness

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -15,73 +15,68 @@ const NavBar = () => {
   };
 
   return (
-    <header className="bg-opacity-0 fixed w-full top-0 z-40">
-      <div className="container mx-auto px-4 h-16">
-        <div className="w-full h-full grid grid-cols-3 items-center justify-center">
-          {/* Left Side */}
-          <div className="flex items-center gap-8">
-            <Link
-              to="/"
-              className="flex items-center gap-3.5 hover:brightness-125 transition-all"
-            >
-              <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
-                <FishSymbol className="w-5 h-5 text-primary" />
-              </div>
-              <h1 className="text-lg font-bold">Catch Logger</h1>
-            </Link>
-          </div>
+    <header className="h-16 w-full bg-opacity-0 top-0 z-40">
+      <div className="h-full grid grid-cols-3">
+        {/* Left Side */}
+        <div className="flex items-center gap-8">
+          <Link
+            to="/"
+            className="flex items-center gap-3.5 hover:brightness-125 transition-all"
+          >
+            <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
+              <FishSymbol className="w-5 h-5 text-primary" />
+            </div>
+            <h1 className="text-lg font-bold hidden lg:block">Catch Logger</h1>
+          </Link>
+        </div>
 
-          {/* Search Bar */}
-          <div className="flex items-center justify-center">
-            {authenticatedUser && (
-              <>
-                <div
-                  className={`relative flex items-center justify-center ${
-                    !isExpanded
-                      ? "w-20 "
-                      : "w-80 text-primary bg-white rounded-full borderborder-gray-300"
-                  } transition-all duration-300  focus-within:shadow-md`}
-                >
-                  <button
-                    onMouseOver={handleExpand}
-                    className="flex items-center justify-center w-10 h-10 text-gray-500"
-                  >
-                    <Search size={20} />
-                  </button>
-                  <input
-                    type="text"
-                    className={`${
-                      isExpanded ? "w-full" : "w-0"
-                    } h-full w-full px-2 text-sm text-gray-700 bg-transparent outline-none transition-all duration-300`}
-                    placeholder="Search"
-                    value={searchText}
-                    onChange={(e) => setSearchText(e.target.value)}
-                    onBlur={handleCollapse}
-                    onFocus={handleExpand}
-                  />
-                </div>
-              </>
-            )}
-          </div>
-
-          {/* Right Side */}
-          <div className="flex items-center gap-2 justify-end ">
-            {authenticatedUser && (
-              <>
-                <Link to={"/profile"} className={`btn btn-sm gap-2`}>
-                  <User className="size-5" />
-                  <span className="hidden sm:inline">Profile</span>
-                </Link>
-
+        {/* Search Bar */}
+        <div className="flex items-center justify-center">
+          {authenticatedUser && (
+            <>
+              <div
+                className={`relative flex items-center justify-center ${
+                  !isExpanded
+                    ? "w-20 "
+                    : "w-80 text-primary bg-white rounded-full borderborder-gray-300"
+                } transition-all duration-300  focus-within:shadow-md`}
+              >
                 <button
-                  className="flex pl-2 gap-2 items-center"
-                  onClick={logout}
+                  onMouseOver={handleExpand}
+                  className="flex items-center justify-center w-10 h-10 text-gray-500"
                 >
-                  <LogOut className="size-6 hover:text-red-500 transition-all duration-300" />
+                  <Search size={20} />
                 </button>
-              </>
-            )}
-          </div>
+                <input
+                  type="text"
+                  className={`${
+                    isExpanded ? "w-full" : "w-0"
+                  } h-full w-full px-2 text-sm text-gray-700 bg-transparent outline-none transition-all duration-300`}
+                  placeholder={isExpanded ? "Search Users" : ""}
+                  value={searchText}
+                  onChange={(e) => setSearchText(e.target.value)}
+                  onBlur={handleCollapse}
+                  onFocus={handleExpand}
+                />
+              </div>
+            </>
+          )}
+        </div>
+
+        {/* Right Side */}
+        <div className="flex items-center gap-2 justify-end ">
+          {authenticatedUser && (
+            <>
+              <Link to={"/profile"} className={`btn btn-sm gap-2`}>
+                <User className="size-5" />
+                <span className="hidden lg:inline">Profile</span>
+              </Link>
+
+              <button className="flex pl-2 gap-2 items-center" onClick={logout}>
+                <LogOut className="size-6 hover:text-red-500 transition-all duration-300" />
+              </button>
+            </>
+          )}
         </div>
       </div>
     </header>
