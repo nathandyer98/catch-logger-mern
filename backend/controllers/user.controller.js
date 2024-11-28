4952d128e135a7c7635163fa9ed8d28273ecaab2
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-11-28 19:00:46 +0000

    updated user.controller
    +now shows username, follower, following

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -5,17 +5,21 @@ export const getUserProfile = async (req, res) => {
     try {
         const userId = req.params.id || req.user._id;
         const user = await User.findById(userId)
-        .select("fullName profilePic catches")
+        .select("fullName username profilePic catches following followers")
         .populate("catches");
 
         if (!user) {
             return res.status(404).json({ message: "User not found." });
         }
 
-        const friends = await User.findById(userId).select("friends")
-
-
-        res.status(200).json({ _id: user._id, fullName: user.fullName, profilePic: user.profilePic, friends: friends.friends.length, catches: user.catches });
+        res.status(200).json({ 
+            _id: user._id, 
+            username: user.username,
+            fullName: user.fullName, 
+            profilePic: user.profilePic, 
+            following: user.following.length, 
+            followers: user.followers.length, 
+            catches: user.catches });
     } catch (error) {
         console.log("Error in getUserProfile controller", error);
         res.status(500).json({ message: "Internal server error" });
