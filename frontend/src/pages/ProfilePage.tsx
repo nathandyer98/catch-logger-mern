10c143f5af3c325b09191f96775be66458ab6689
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-11-28 23:12:16 +0000

    updated profilePage
    +fetches user data based on search
    +stats slider on profile click

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,56 +1,76 @@
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useProfileStore } from "../store/useProfileStore";
 import { Fish, MapPin, Trophy } from "lucide-react";
+import { useParams } from "react-router-dom";
 
-interface ProfilePageProps {
-  userId?: string; // Optional userId to fetch a specific profile
-}
-
-const ProfilePage: React.FC<ProfilePageProps> = ({ userId }) => {
+const ProfilePage = () => {
   const { selectedUser, isLoading, fetchProfile } = useProfileStore();
 
+  const { username } = useParams();
+
+  //CLICKING PROFILE PIC REVEALS STATS
+  const [showStats, setShowStats] = useState(false);
+
   useEffect(() => {
-    fetchProfile(userId);
-  }, [userId, fetchProfile]);
+    fetchProfile(username);
+  }, [fetchProfile, username]);
 
   if (isLoading) return <div>Loading...</div>;
 
   console.log(selectedUser);
+  console.log(username);
 
   return (
-    <div className="pt-24 p-4">
-      <div className="text-center">
-        <img
-          src={selectedUser?.profilePic || "/avatar.png"}
-          alt="Profile"
-          className="w-32 h-32 rounded-full mx-auto mb-4"
-        />
-        <h2 className="text-2xl font-bold">{selectedUser?.fullName}</h2>
-        <div className="flex justify-center space-x-4 mt-2">
-          <div>Followers: {selectedUser?.friends}</div>
-          <div>Following: 178</div>
+    <>
+      <div className="p-6 flex justify-center items-center">
+        {/* Stats Slider*/}
+        <div
+          className={`absolute transition-transform-reverse duration-500 
+        ${
+          showStats
+            ? " opacity-100 brightness-100  translate-x-80 "
+            : "opacity-0 brightness-0 translate-x-30"
+        }`}
+        >
+          <h3 className="text-xl font-semibold mb-4">Personal Stats</h3>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="bg-gray-100 p-3 rounded">
+              <Trophy className="inline mr-2" />
+              Total Fish: 0
+            </div>
+            <div className="bg-gray-100 p-3 rounded">
+              <Fish className="inline mr-2" />
+              Personal Best: 0 kg
+            </div>
+            <div className="bg-gray-100 p-3 rounded">
+              <MapPin className="inline mr-2" />
+              Favourite Lake:
+            </div>
+            <div className="bg-gray-100 p-3 rounded">Favourite Hookbait:</div>
+          </div>
+        </div>
+
+        {/* Profile Pic */}
+        <div
+          className={`text-center transition-transform-reverse duration-500 ${
+            showStats ? "-translate-x-60 " : "-translate-x-0 "
+          }`}
+        >
+          <img
+            onClick={() => setShowStats(!showStats)}
+            src={selectedUser?.profilePic || "/avatar.png"}
+            alt="Profile"
+            className="w-32 h-32 rounded-full mx-auto mb-4"
+          />
+          <h2 className="text-2xl font-bold">{selectedUser?.fullName}</h2>
+          <div className="flex justify-center space-x-4 mt-2">
+            <div>Followers: {selectedUser?.friends}</div>
+            <div>Following: 178</div>
+          </div>
         </div>
       </div>
 
-      <div className="mt-6">
-        <h3 className="text-xl font-semibold mb-4">Personal Stats</h3>
-        <div className="grid grid-cols-2 gap-4">
-          <div className="bg-gray-100 p-3 rounded">
-            <Trophy className="inline mr-2" />
-            Total Fish: 0
-          </div>
-          <div className="bg-gray-100 p-3 rounded">
-            <Fish className="inline mr-2" />
-            Personal Best: 0 kg
-          </div>
-          <div className="bg-gray-100 p-3 rounded">
-            <MapPin className="inline mr-2" />
-            Favourite Lake:
-          </div>
-          <div className="bg-gray-100 p-3 rounded">Favourite Hookbait:</div>
-        </div>
-      </div>
-
+      {/* Catch Filters ---PROVISIONAL--- */}
       <div className="mt-6">
         <h3 className="text-xl font-semibold mb-4">Catch Filters</h3>
         <div className="flex space-x-4 mb-4">
@@ -77,6 +97,7 @@ const ProfilePage: React.FC<ProfilePageProps> = ({ userId }) => {
         </div>
       </div>
 
+      {/* My Catches ----NEED TO IMPLEMENT----*/}
       <div className="mt-6">
         <h3 className="text-xl font-semibold mb-4">My Catches</h3>
         {selectedUser?.catches?.length ? (
@@ -90,7 +111,7 @@ const ProfilePage: React.FC<ProfilePageProps> = ({ userId }) => {
           <p>No catches yet.</p>
         )}
       </div>
-    </div>
+    </>
   );
 };
 
