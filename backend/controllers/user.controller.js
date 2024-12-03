ef0dde7681f5e95ca7f2594fda14ac8fa11f5ec9
Nathan Dyer <nathanjohndyer@live.co.uk>
2024-12-03 15:03:45 +0000

    separated backend user logic
    +get calls to getCurrentProfile to retrieve the current user's profile and other private info
    +getUsersProfile to retrieve another user's data to display in profile view.

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,11 +1,36 @@
 import User from "../models/user.model.js";
 
 
-export const getUserProfile = async (req, res) => {
-    try {
-        const username = req.params.username || req.user.username; 
 
-        const user = await User.findOne({ username })
+export const getCurrentProfile = async (req, res) => {
+    try {
+            const user = await User.findOne(req.user._id)
+            .select("fullName username profilePic catches following followers") 
+            .populate("catches");
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found." });
+        }
+
+        res.status(200).json({ 
+            _id: user._id, 
+            username: user.username,
+            fullName: user.fullName, 
+            profilePic: user.profilePic, 
+            following: user.following.length, 
+            followers: user.followers.length, 
+            catches: user.catches });
+    } catch (error) {
+        console.log("Error in getUserProfile controller", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const getUsersProfile = async (req, res) => {
+    try {
+        const selectedUser = req.params.username 
+
+        const user = await User.findOne({ selectedUser })
             .select("fullName username profilePic catches following followers")
             .populate("catches");
 
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,14 +1,14 @@
 import express from "express";
 
-import { getUserProfile } from "../controllers/user.controller.js";
+import { getCurrentProfile, getUsersProfile } from "../controllers/user.controller.js";
 import { authenticatedRoute } from "../middleware/auth.middleware.js";
 
 
 const router = express.Router();
 
-router.get("/me", authenticatedRoute, getUserProfile);
+router.get("/me", authenticatedRoute, getCurrentProfile);
 
-router.get("/:username", authenticatedRoute, getUserProfile);
+router.get("/:username", authenticatedRoute, getUsersProfile);
 
 // router.post("/:id/follow", authenticatedRoute, followUser);
 
