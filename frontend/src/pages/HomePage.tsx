db3ecd527bad6e41ec5657e87ff8aeab90056d4a
Nathan Dyer <nathanjohndyer@live.co.uk>
2025-01-08 20:11:56 +0000

    updated Home Page
    +home page now contains the create post and post widget components.

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
 import CreatePostWidget from "../components/CreatePostWidget";
+import { useCatchStore } from "../store/useCatchStore";
+import PostWidget from "../components/PostWidget";
 
 const HomePage = () => {
+  const { feedCatches, isFetchingCatches, fetchCatchesFeed } = useCatchStore();
+
+  useEffect(() => {
+    fetchCatchesFeed();
+  }, [fetchCatchesFeed]);
+
+  if (isFetchingCatches) return <div>Loading...</div>;
+
   return (
-    <div className="">
+    <div className="space-y-6">
       <CreatePostWidget />
+      {!feedCatches && <div>No catches found to display :( </div>}
+      {feedCatches &&
+        feedCatches.map((catchLog, index) => (
+          <PostWidget catchData={catchLog} key={index + 1} />
+        ))}
     </div>
   );
 };
