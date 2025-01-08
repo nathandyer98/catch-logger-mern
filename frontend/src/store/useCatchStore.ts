7a5a67f77be0f3a15049e6875bc5b1432be67839
Nathan Dyer <nathanjohndyer@live.co.uk>
2025-01-08 20:09:48 +0000

    initialised a CatchStore
    +handling all CRUD actions relating to catches, as well as retrieval of catches for a specified user.

---
+++ frontend/src/store/useCatchStore.ts
@@ -0,0 +1,65 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { create } from "zustand";
+import { axiosInstance } from "../services/api-client";
+import { CatchFormData } from "../types/forms";
+import { Catch } from "../types/catches";
+import toast from "react-hot-toast";
+
+interface CatchState {
+    feedCatches: Catch[] | null;
+    userCatches: Catch[] | null;
+
+    isAddingCatch: boolean;
+    isFetchingCatches: boolean;
+
+    addCatch: (formData: CatchFormData) => Promise<void>;
+    fetchUserCatches: (username: string) => Promise<void>;
+    fetchCatchesFeed: () => Promise<void>;
+}
+
+export const useCatchStore = create<CatchState>((set) => ({
+    feedCatches: null,
+    userCatches: null,
+
+    isAddingCatch: false,
+    isFetchingCatches: false,
+    addCatch: async (formData: CatchFormData) => {
+        set({ isAddingCatch: true });
+        try {
+            await axiosInstance.post("/catches", formData);
+            toast.success("Catch added successfully");
+        } catch (error: any) {
+            console.log("Error in add catch controller", error);
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isAddingCatch: false });
+        }
+    },
+
+    fetchUserCatches: async (username: string) => {
+        set({ isFetchingCatches: true });
+        try {
+            const res = await axiosInstance.get(`/catches/user/${username}`);
+            set({ userCatches: res.data });
+        } catch (error: any) {
+            console.log("Error in fetchCatches controller", error);
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isFetchingCatches: false });
+        }
+    },
+
+    fetchCatchesFeed: async () => {
+        set({ isFetchingCatches: true });
+        try {
+            const res = await axiosInstance.get("/catches/feed");
+            set({ feedCatches: res.data });
+        } catch (error: any) {
+            console.log("Error in fetchCatches controller", error);
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isFetchingCatches: false });
+        }
+    },
+
+}));
