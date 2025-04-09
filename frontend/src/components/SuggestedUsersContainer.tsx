e3c51af29f7c3fc483cec57dba08dea184724a11
Nathan Dyer <nathanjohndyer@live.co.uk>
2025-04-09 14:18:48 +0100

    fixed and renamed: suggested users
    +fixed an issue where it was not correctly pulling unfollowed users
    +renamed file to for better readability
    +created some skeletons // Not added because it was creating popping

--- frontend/src/components/SuggestedUsersContainer.tsx
+++ frontend/src/components/SuggestedUsersContainer.tsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import { useProfileStore } from "../store/useProfileStore";
 import { useEffect } from "react";
+import { RotateCcw } from "lucide-react";
 
-const SuggestedUsers = () => {
+const SuggestedUsersContainer = () => {
   const {
-    isFetchingSuggestedUser,
+    // isFetchingSuggestedUser,
     suggestedUsers,
     fetchSuggestedUsers,
     followUnfollowUser,
@@ -18,17 +19,16 @@ const SuggestedUsers = () => {
     fetchSuggestedUsers();
   }, [fetchSuggestedUsers]);
 
-  if (isFetchingSuggestedUser) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className="hidden lg:block mb-4 mx-2">
       <div className=" rounded-lg top-2 ">
-        <p className="font-bold mb-4">Who to follow</p>
+        <div className="flex items-center justify-between p-3 relative">
+          <p className="font-bold mb-4 text-white/90">Who to follow</p> 
+          <RotateCcw className="w-6 h-6 absolute right-3 cursor-pointer hover:opacity-70" onClick={fetchSuggestedUsers}/>
+        </div>
         <div className="flex flex-col gap-2">
           {/* Item */}
-          {suggestedUsers &&
+          { suggestedUsers &&
             suggestedUsers?.map((user) => (
               <div
                 className="p-3 flex items-center justify-between hover:bg-base-300 hover:rounded-xl"
@@ -42,7 +42,7 @@ const SuggestedUsers = () => {
                       </div>
                     </div>
                     <div className="flex flex-col">
-                      <span className="font-semibold tracking-tight truncate w-28">
+                      <span className="font-semibold tracking-tight truncate w-28 text-white/90 ">
                         {user.fullName}
                       </span>
                       <span className="text-sm text-slate-500">
@@ -56,8 +56,8 @@ const SuggestedUsers = () => {
                   <button
                     className={`text-black btn rounded-full btn-sm ${
                       user.isFollowing
-                        ? " bg-blue-500 hover:bg-blue-500/90 "
-                        : "bg-white hover:bg-white hover:opacity-90"
+                        ? " bg-blue-500 text-white hover:bg-blue-500/90 "
+                      : "bg-white text-black  hover:bg-white hover:opacity-90"
                     }`}
                     onClick={() => handleFollowUser(user._id)}
                   >
@@ -71,4 +71,4 @@ const SuggestedUsers = () => {
     </div>
   );
 };
-export default SuggestedUsers;
+export default SuggestedUsersContainer;
